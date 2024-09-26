bae6a0b2c16b153b9b7f66e56dda7d7ac6855e4d
bhavyam3011 <22ucs049@lnmiit.ac.in>
2024-09-27 05:11:18 +0530

    Update tsk14.py
    Updated the Dataset for the Web Page

--- tsk14.py
+++ tsk14.py
@@ -1,47 +1,17 @@
 import streamlit as st
 import pandas as pd
-# import matplotlib as plt
+import numpy as np
 
-# st.header("Startup Funding Dashboard")
-df = pd.read_csv('startup_cleaned.csv')
-# st.dataframe(df)
-# df['Investors Name'] = df['Investors Name'].fillna("undisclosed")
-# df['month'] = df['Date'].dt.month
-# df['year'] = df['Date'].dt.year
+st.header("Credit card Fraud Detection")
+df = pd.read_csv('creditcard.csv')
+st.dataframe(df)
 
-def load_investor_details(investor):
-    # names
-    st.header(investor)
-    # recent events
-    last5=df[df['Investors Name'].str.contains(investor)].head()[['Date','Startup','Vertical','City','Amount']] 
-    st.subheader("Recent Events")
-    st.dataframe(last5)
+legit = df[df['Class'] == 0]
+fraud = df[df['Class'] == 1]
 
-    # biggest investment 
-    big=df[df['Investors Name'].str.contains('DG Ventures')].groupby('Startup')['Amount'].sum().sort_values(ascending=False).head(3) 
-    st.subheader("Biggest Investments")
-    st.dataframe(big)
-    #
-    # general invests 
-
-# st.sidebar.title("Startup funding analysis")
-option=st.sidebar.selectbox("select one: ",['OverAll','StartUp','Investor'])
-
-if option == 'OverAll':
-    st.title('OverAll')
-
-
-elif option == 'StartUp':
-    select1=st.sidebar.selectbox("Select StartUp", set(df['Startup'].str.split(",").sum()))
-    btn1=st.sidebar.button("find details")
-    st.title('StartUp')
-
-elif option == 'Investor':
-    select2=st.sidebar.selectbox("Select investor", set(df['Investors Name'].str.split(",").sum()))
-    btn2=st.sidebar.button("find details")
-    st.title("Investor Analysis")
-
-    if btn2:
-        load_investor_details(select2)
-
-    
+st.subheader("Legit Transactions")
+st.dataframe(legit)
+st.dataframe(legit.describe())
+st.subheader("Fraudulent Transactions")
+st.dataframe(fraud)
+st.dataframe(fraud.describe())
