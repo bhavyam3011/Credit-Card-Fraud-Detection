aa538a8b196522188b48a2e4465882c9065ffcf4
bhavyam3011 <22ucs049@lnmiit.ac.in>
2024-11-13 17:37:21 +0530

    Update tsk14.py

--- tsk14.py
+++ tsk14.py
@@ -10,7 +10,7 @@ st. set_page_config(layout="wide")
 #      columns=['a', 'b', 'c'])
 
 # st.line_chart(chart_data)
-df = pd.read_csv('creditcard.csv')
+df = pd.read_csv('modelling_set.csv')
 X = df.drop('Class', axis=1)
 
 y = df['Class']
